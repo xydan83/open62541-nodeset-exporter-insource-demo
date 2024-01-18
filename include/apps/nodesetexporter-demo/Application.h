4d180540d843550d1a0da3062213f32728bdc514
xydan83 <aleksprog@hotmail.com>
2024-01-18 21:59:15 +0300

    Added an example of including and using the nodesetexporter (and open62541) libraries as submodules.

---
+++ include/apps/nodesetexporter-demo/Application.h
@@ -0,0 +1,78 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright 2024 (c) Aleksander Rozhkov <aleksprog@hotmail.com>
+//
+
+#ifndef APPS_NODESETEXPORTERDEMO_APPLICATION_H
+#define APPS_NODESETEXPORTERDEMO_APPLICATION_H
+
+
+#include <LoggerBase.h>
+#include <NodesetExporter.h>
+
+#include <span>
+
+namespace apps::nodesetexporter::demo
+{
+
+class InterruptException : public std::runtime_error
+{
+public:
+    InterruptException() = delete;
+    explicit InterruptException(const std::string& arg)
+        : runtime_error(arg)
+    {
+    }
+};
+
+class Application
+{
+
+public:
+    Application() = delete;
+
+    explicit Application(std::span<const char*> args)
+        : m_args(args)
+        , m_client(nullptr)
+        , m_client_endpointUrl(m_args[1])
+    {
+        m_start_node_ids.emplace_back(std::string(m_args[2]));
+    }
+
+    ~Application()
+    {
+        if (m_client != nullptr)
+        {
+            UA_Client_delete(m_client);
+        }
+    }
+
+    /**
+     * @brief Start export operation
+     */
+    void StartExport();
+
+    /**
+     * @brief Initialization and startup process.
+     * @return EXIT_SUCCESS in case of normal shutdown or EXIT_FAILURE.
+     */
+    int Run();
+
+private:
+    std::span<const char*> const m_args{};
+
+    UA_Client* m_client;
+
+    std::string m_export_filename = "demo_export.xml";
+    std::string m_client_endpointUrl{};
+    std::vector<std::string> m_start_node_ids{};
+    ::nodesetexporter::Options m_opt{};
+};
+
+} // namespace apps::nodesetexporter::demo
+
+
+#endif // APPS_NODESETEXPORTERDEMO_APPLICATION_H
---
+++ src/apps/nodesetexporter-demo/Application.cpp
@@ -0,0 +1,99 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright 2024 (c) Aleksander Rozhkov <aleksprog@hotmail.com>
+//
+
+#include "apps/nodesetexporter-demo/Application.h"
+#include "apps/nodesetexporter-demo/BrowseOperations.h"
+
+
+#include <Encoder_types.h>
+#include <Statuses.h>
+#include <UATypesContainer.h>
+
+#include <open62541/client.h>
+#include <open62541/client_config_default.h>
+
+#include <iostream>
+
+namespace apps::nodesetexporter::demo
+{
+using ::nodesetexporter::open62541::UATypesContainer;
+using StatusResults = ::nodesetexporter::common::statuses::StatusResults;
+using LogLevel = ::nodesetexporter::common::LogLevel;
+using EncoderTypes = ::nodesetexporter::common::EncoderTypes;
+using ::nodesetexporter::ExportNodesetFromClient;
+
+void Application::StartExport()
+{
+    // The first main operation is collecting units for export. Can take a long time.
+    std::cout << "Browse node lists for export" << std::endl;
+    std::map<std::string, std::vector<UATypesContainer<UA_ExpandedNodeId>>> node_ids_export;
+
+    // If we can have many starting nodes
+    for (const auto& start_node_id_s : m_start_node_ids)
+    {
+        // Use special C++ wrapper (UATypesContainer) for UA objects
+        std::vector<UATypesContainer<UA_ExpandedNodeId>> export_node_id_list;
+        UATypesContainer<UA_ExpandedNodeId> start_node_id(UA_EXPANDEDNODEID(start_node_id_s.data()), UA_TYPES_EXPANDEDNODEID);
+        // Browse nodes from start node
+        auto client_result = browseoperations::GrabChildNodeIdsFromStartNodeId(m_client, start_node_id, export_node_id_list);
+        std::cout << "Browsing operation from starting NodeID " << start_node_id_s << std::endl;
+        if (!UA_StatusCode_isGood(client_result))
+        {
+            throw std::runtime_error("Browsing error: " + std::string(UA_StatusCode_name(client_result)));
+        }
+
+        node_ids_export.emplace(start_node_id_s, std::move(export_node_id_list));
+    }
+
+    // The second main operation is export. Nodesetexporter library function. Can take a long time.
+    std::cout << "Launch export" << std::endl;
+    auto nodeexporter_status = ExportNodesetFromClient(*m_client, node_ids_export, std::move(m_export_filename), std::nullopt, m_opt);
+    if (nodeexporter_status != StatusResults::Good)
+    {
+        throw std::runtime_error("Export error");
+    }
+}
+
+int Application::Run()
+{
+    try
+    {
+        UA_StatusCode client_result = UA_STATUSCODE_GOOD;
+
+        // Preparing auxiliary export options
+        m_opt.internal_log_level = LogLevel::Info;
+        m_opt.encoder_types = EncoderTypes::XML;
+
+        m_client = UA_Client_new();
+        std::cout << "Configuration the Open62541 Client" << std::endl;
+        auto* cli_config = UA_Client_getConfig(m_client);
+        UA_ClientConfig_setDefault(cli_config);
+
+        std::cout << "Connecting a Client to a Server" << std::endl;
+        client_result = UA_Client_connect(m_client, m_client_endpointUrl.data());
+
+        if (!UA_StatusCode_isGood(client_result))
+        {
+            std::cout << "OPC UA Client error: " << UA_StatusCode_name(client_result) << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        StartExport();
+
+        std::cout << "I`m leaving..." << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+} // namespace apps::nodesetexporter::demo
